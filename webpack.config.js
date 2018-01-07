07fcf63fddfae5356a243d0a84a82b7732020e88
JH Foo <foojihaw@hotmail.com>
2018-01-06 22:55:06 -0600

    first codes (non functional)

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path'),
+    webpack = require('webpack'),
+    WebpackAutoInject = require('webpack-auto-inject-version'),
+    BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build/js'),
+        publicPath: '/js/',
+        chunkFilename: '[name]-bundle.js',
+        filename: '[name]-bundle.js'
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    // vendor: ['axios'],
+    plugins: [
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common' // Specify the common bundle's name.
+        // }),
+        new WebpackAutoInject({
+            components: {
+                AutoIncreaseVersion: true,
+                InjectByTag: true
+            },
+            componentsOptions: {
+                AutoIncreaseVersion: {
+                    runInWatchMode: true
+                },
+                InjectByTag: {
+                    dateFormat: 'h:MM:ss TT'
+                }
+            }
+        })
+        // new BundleAnalyzerPlugin({
+        //     analyzerMode: 'static'
+        // })
+    ],
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+
+            }
+        }],
+        loaders: []
+    }
+};
